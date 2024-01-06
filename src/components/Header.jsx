1bb56e10eed50b395f72e8b7daaf247b96e0c052
Soroush Ghasemi <soroush.341379@gmail.com>
2024-01-06 17:09:05 +0330

    added Stylex to Header Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,51 @@
-import styles from "./Header.module.css";
+import * as stylex from "@stylexjs/stylex";
+
+// import styles from "./Header.module.css";
 import lightTheme from "../assets/light-theme.png";
 import darkTheme from "../assets/dark-theme.png";
+
+const styles = stylex.create({
+    container: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        borderRadius: "10px",
+        padding: "10px",
+        marginBottom: "80px",
+    },
+    dark: {
+        backgroundColor: "#2b2b2b",
+        color: "#e0e0e0",
+    },
+    light: {
+        backgroundColor: "#b75b8ecc",
+        color: "#2b2b2b",
+    },
+    p: {
+        display: "flex",
+        alignItems: "center",
+    },
+    a: {
+        marginRight: "3px",
+        textDecoration: "none",
+        fontWeight: "700",
+    },
+    darkBoto: {
+        color: "#dc74aecc",
+    },
+    lightBoto: {
+        color: "#660d41",
+    },
+    img: {
+        height: "40px",
+        padding: {
+            default: "10px",
+            ":hover": "7px",
+        },
+        cursor: "pointer",
+        marginLeft: "10px",
+    },
+});
 
 const images = {
     darkTheme,
@@ -9,14 +54,18 @@ const images = {
 
 function Header({ theme, setTheme }) {
     return (
-        <div className={`${styles.container} ${styles[theme]}`}>
+        <div {...stylex.props(styles.container, styles[theme])}>
             <h1>Library App</h1>
-            <p>
-                <a href="https://botostart.ir" className={styles[`${theme}Boto`]}>
+            <p {...stylex.props(styles.p)}>
+                <a
+                    href="https://botostart.ir"
+                    {...stylex.props(styles.a, styles[`${theme}Boto`])}
+                >
                     Botostart
                 </a>
                 | React.js Full Course
                 <img
+                    {...stylex.props(styles.img)}
                     src={images[`${theme}Theme`]}
                     onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
                 />
